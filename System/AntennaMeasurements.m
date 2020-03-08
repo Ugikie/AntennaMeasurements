4a2badc152e2390d832e0c175f257bd4d882d38a
Austin Adam <austinadam42@gmail.com>
2020-03-07 19:38:17 -0800

    Added control for the Roll Axis on the MI4190 and almost completed the data collection. Can now save data into a .mat file for single and span frequency measurements, just has to be tested. Also made some improvements to the UI.

--- System/AntennaMeasurements.m
+++ System/AntennaMeasurements.m
@@ -1,8 +1,7 @@
 clear all; clc;
 
 
-%measApp = measApp();
-
+measApp = measApp();
 
 %Use this for testing creating a data structure containing 
 %{freqIntervalStart ... freqIntervalStop}
@@ -17,13 +16,14 @@ clear all; clc;
 %startFreqMat(:,:,2) = [degInterval' 2*S11]
 %startFreqMat(:,:,3) = [degInterval' 3*S11]
 %dataToWrite = {startFreqMat startFreqMat}
-dataToWrite = {1};
+
+dataToWrite = {datestr(now)};
 x = readtable('newMeasurement_16:14:36.csv');
 S11 = x.Var3;
 azInterval = -90:5:90;
 rollInterval = -90:5:90;
 
-freqInterval = 1.00:10.00;
+freqInterval = 1.00:0.5:10.00;
 
 for j = 1:size(freqInterval,2)
     rollIntervalOnesMat = ones(size(rollInterval',1),1);
@@ -35,9 +35,37 @@ for j = 1:size(freqInterval,2)
     dataToWrite = {dataToWrite{1:end} currentFreqCell};
 end
 
+
+
+% for freq = freqInterval
+%     [~,idx] = find(freqInterval == freq);
+%     varName = [num2str(freqInterval(idx)) ' GHz'];
+%     
+%     nccreate('data.nc',varName,'Dimensions', {'x',size(dataToWrite{2},1),'y',size(dataToWrite{2},2),'z',size(dataToWrite{2},3)});
+%     ncwrite('data.nc',varName,dataToWrite{2},[1 1 1]);
+% end
+% 
+% ncdisp('data.nc');
+
+
+%Turns dataToWrite into a table for writing in a csv file.
+%T = cell2table(dataToWrite(1:end,:));
+
+% Write the table to a CSV file
+%writetable(T,'myDataFile.csv')
+
+
+
+
 freqToInspect = 5.00;
 % 
 % for i = 1:size(dataToWrite,2)
 %     S21ValFromData = 
 % end
-plot(dataToWrite{6}(:,3,1),20*log10(abs(dataToWrite{6}(:,4,1))),dataToWrite{6}(:,3,2),20.1*log10(abs(dataToWrite{6}(:,4,2))),dataToWrite{6}(:,3,3),20.2*log10(abs(dataToWrite{6}(:,4,3))))
+
+
+% hold on
+% for i = 1:size(dataToWrite{6},3)
+%     plot(dataToWrite{6}(:,3,i),20*log10(abs(dataToWrite{6}(:,4,i))))
+% end
+
